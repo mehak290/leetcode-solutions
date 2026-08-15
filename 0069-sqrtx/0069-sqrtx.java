93615c97721d6c8fea6605ab30ed1c6ecea2922a
Mehak Sampla <mehaksampla10@gmail.com>
2026-08-16 01:12:31 +0530

    Time: 1 ms (98.13%), Space: 42.5 MB (61.93%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int mySqrt(int x) {
+        int left=0;
+        int right=x;
+        int mid =0;
+        while(left<=right){
+            mid=left+(right-left)/2;
+            if((long)mid*mid==x) return mid;
+            else if((long)mid*mid>x) right=mid-1;
+            else{
+                left=mid+1;
+            }
+        }
+        return right;
+    }
+}
